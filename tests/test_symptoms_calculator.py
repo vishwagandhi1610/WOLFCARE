e6322aa200ce85c15a47bf34a85f3b786f0ebdb3
Dhruv Patel <54198410+dhruvpatel-9@users.noreply.github.com>
2022-10-07 19:27:00 -0400

    Update test_symptoms_calculator.py

--- tests/test_symptoms_calculator.py
+++ tests/test_symptoms_calculator.py
@@ -11,25 +11,25 @@ a = symptoms_calculator.symptomsCalculator()
 class testReturnValues(unittest.TestCase):
     
     def test_acidBaseDisorder(self):
-        temp1, temp2 = a.acidBaseDisorder(15, 31)
+        temp1, temp2 = a.acidBaseDisorder(15, 31) # pragma: no cover
         self.assertEqual(temp1, True) and self.assertEqual(temp2, False)
     
     def test_hyperCholesterolemia(self):
-        temp = a.hyperCholesterolemia(160)
+        temp = a.hyperCholesterolemia(160) # pragma: no cover
         self.assertEqual(temp, False)
     
     def test_hypertension(self):
-        temp = a.hypertension(145, 95)
-        self.assertEqual(temp, True)
+        temp = a.hypertension(145, 95) # pragma: no cover
+        self.assertEqual(temp, True) # pragma: no cover
     
     def test_kidneyInjury(self):
-        temp1, temp2, temp3, temp4 = a.kidneyInjury(1.5, 1.1)
-        if ((temp1 == True) and (temp2 == False) and (temp3 == False) and (temp4 == True)):
+        temp1, temp2, temp3, temp4 = a.kidneyInjury(1.5, 1.1) # pragma: no cover
+        if ((temp1 == True) and (temp2 == False) and (temp3 == False) and (temp4 == True)): # pragma: no cover
             temp = True
-        else:
+        else: # pragma: no cover
             temp = False
-        self.assertEqual(temp, True)
+        self.assertEqual(temp, True) # pragma: no cover
     
     def test_obesity(self):
-        temp = a.obesity(180, 72)
-        self.assertEqual(temp, False)
+        temp = a.obesity(180, 72) # pragma: no cover
+        self.assertEqual(temp, False) # pragma: no cover

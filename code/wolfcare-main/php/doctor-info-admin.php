18843f402af9b142811f92b6678f072661817aee
Vrushanki_09 <vpatel25@ncsu.edu>
2022-12-02 16:59:38 -0500

    doctor information

---
+++ code/wolfcare-main/php/doctor-info-admin.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../stylesheets/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="../stylesheets/show_appointments.css" />
+</head>
+</html>

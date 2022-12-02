12a3a406bc348c830f9ac17f7644c20d1a917737
Nirav Shah <nshah28@ncsu.edu>
2022-12-02 17:50:41 -0500

    Added doctor info admin

---
+++ code/wolfcare-main/php/get-doctor-info-admin.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+    $servername="localhost";
+    $username="root";
+    $password = "";
+    $dbname="kj_mini";
+    $conn = mysqli_connect($servername,$username,$password,$dbname);
+    $sql = "SELECT * FROM doctors 
+            WHERE doc_id = '$_GET['book-btn'];'";
+    $row = mysqli_fetch_assoc(mysqli_query($conn,$sql));
+    $_SESSION['doc_info'] = $row;
+    echo $_SESSION['doc_info']['doc_lname'];
+    header('location: doctor-info-admin.php'); 
+?>

24fe328dc3b372654b3cc8da5e143295a74513b7
Nirav Shah <nshah28@ncsu.edu>
2022-12-02 16:54:40 -0500

    Updating doctor data

---
+++ code/wolfcare-main/php/doctor_update.php
@@ -0,0 +1,21 @@
+<?php
+    if (isset($_POST['done'])){
+        session_start();
+        $user = $_SESSION['doctor_id'];
+        $servername="localhost";
+        $username="root";
+        $password = "";
+        $dbname="kj_mini";
+        $conn = mysqli_connect($servername,$username,$password,$dbname);
+        $firstName = $_POST['firstName'];
+        $lastName = $_POST['lastName'];
+        //$username = $_POST['userName'];
+        $email = $_POST['email'];
+        $number = $_POST['number'];
+        $doc_type = $_POST['doc_type'];
+        $location = $_POST['location'];
+        $sql = "UPDATE doctors SET doc_fname='$firstName',doc_lname='$lastName',doc_phone='$number',email='$email', doc_location='$location',doc_type='$doc_type' WHERE doc_id='$user'";
+
+    }
+    
+?>

f7843755d16fc0b46e94482d0068978c11bd2645
Nirav Shah <nshah28@ncsu.edu>
2022-12-02 16:55:09 -0500

    updated doctor profile

--- code/wolfcare-main/php/doctor_update.php
+++ code/wolfcare-main/php/doctor_update.php
@@ -15,7 +15,13 @@
         $doc_type = $_POST['doc_type'];
         $location = $_POST['location'];
         $sql = "UPDATE doctors SET doc_fname='$firstName',doc_lname='$lastName',doc_phone='$number',email='$email', doc_location='$location',doc_type='$doc_type' WHERE doc_id='$user'";
-
+        $query = mysqli_query($conn,$sql);
+        
+        echo $query;
+        echo '<script type="text/javascript">'; 
+        echo 'alert("Profile Updated");'; 
+        echo 'window.location.href = "doctor_profile.php";';
+        echo '</script>';
     }
     
 ?>

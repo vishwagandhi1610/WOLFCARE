8787698c06c1f7e7d8233ea33bc798f6830e1f12
Nirav Shah <nshah28@ncsu.edu>
2022-12-02 18:14:50 -0500

    validate admin

---
+++ code/wolfcare-main/php/validate_admin.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    include 'functions.php';
+    create_database();
+    $email = $_POST['email'];
+    $_SESSION['homepage'] = $email;
+    $query = $email;
+    homepage_user_info();
+    $passwords = $_POST['passwords'];
+    $conn = mysqli_connect("localhost","root","");
+    mysqli_select_db($conn,'kj_mini');
+    $sql = "select email,pass_word from admin where email='$email' && pass_word='$passwords'";
+    $result = mysqli_query($conn,$sql);
+    $num = mysqli_num_rows($result);
+    if($num==1){
+        header('location: admin_home.php');
+    }
+    else{
+        
+    }
+
+?>

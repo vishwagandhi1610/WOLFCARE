fcb0680c0689bda643ac5c50bbbdfa82eb9192a0
Niravsshaha2 <111940049+Niravsshaha2@users.noreply.github.com>
2022-12-05 21:46:10 -0500

    Update test_symptoms_calculator.py

--- tests/test_symptoms_calculator.py
+++ tests/test_symptoms_calculator.py
@@ -1,1 +1,16 @@
 # test file
+import sys
+sys.path.append("../code/")
+
+import pytest
+import unittest
+
+import symptoms_calculator
+
+a = symptoms_calculator.symptomsCalculator()
+
+class testReturnValues(unittest.TestCase):
+    
+    def test_acidBaseDisorder(self):
+        temp1, temp2 = a.acidBaseDisorder(15, 31) # pragma: no cover
+        self.assertEqual(temp1, True) and self.assertEqual(temp2, False)

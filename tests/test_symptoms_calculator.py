886b8043e95d6dcb40c825ba35fe69120765ebb4
VrushankiPatel <111774275+VrushankiPatel@users.noreply.github.com>
2022-12-05 21:47:04 -0500

    Update test_symptoms_calculator.py

--- tests/test_symptoms_calculator.py
+++ tests/test_symptoms_calculator.py
@@ -14,3 +14,8 @@ class testReturnValues(unittest.TestCase):
     def test_acidBaseDisorder(self):
         temp1, temp2 = a.acidBaseDisorder(15, 31) # pragma: no cover
         self.assertEqual(temp1, True) and self.assertEqual(temp2, False)
+        
+    def test_kidneyInjury(self):
+        temp1, temp2, temp3, temp4 = a.kidneyInjury(1.5, 1.1) # pragma: no cover
+        if ((temp1 == True) and (temp2 == False) and (temp3 == False) and (temp4 == True)): # pragma: no cover
+            temp = True
